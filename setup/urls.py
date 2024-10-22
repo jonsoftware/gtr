c88c27e30cee81d016553b14e204774d89cc3450
jonas <jonas.bolado@gmail.com>
2024-10-22 19:17:15 -0300

    atualizando mais calmo 19:17

--- setup/urls.py
+++ setup/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from todos.views import TodoListView, TodoUpdateView
+from todos.views import TodoListView, TodoUpdateView,TodoDeleteView, TodoCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),  # Lista de todos
+    path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>/", TodoUpdateView.as_view(), name="todo_update"),  # Edição de um todo
-    
+    path("delete/<int:pk>/", TodoDeleteView.as_view(), name="todo_delete"),
 
 ]
 
--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import Todo
 
 class TodoListView(ListView):
     model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
 
 class TodoUpdateView(UpdateView):
     model = Todo
@@ -16,16 +21,4 @@ class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
     
-    
-                           
-# Você pode manter a função `todo_list_old` para referência, mas não é mais necessária:
-def todo_list_old(request):
-    nome = 'Rodrigo'
-    alunos = ['1. Ana', '2. José', '3. Bia']
-    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
-
-# View para listar todos os todos
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
-
+ 
